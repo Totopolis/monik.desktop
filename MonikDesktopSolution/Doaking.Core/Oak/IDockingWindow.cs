213a4d4e5f99fcd42730361691f768574ed53df9
Kibnet <platonov@smartdriving.io>
2017-01-13 18:01:40 +0300

    Cleanup code

--- MonikDesktopSolution/Doaking.Core/Oak/IDockingWindow.cs
+++ MonikDesktopSolution/Doaking.Core/Oak/IDockingWindow.cs
@@ -1,16 +1,11 @@
 ﻿using ReactiveUI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MonikDesktop.Oak
 {
-  public interface IDockingWindow
-  {
-    string Title { get; set; }
-    bool CanClose { get; set; }
-    ReactiveCommand CloseCommand { get; set; }
-  }
+	public interface IDockingWindow
+	{
+		string Title { get; set; }
+		bool CanClose { get; set; }
+		ReactiveCommand CloseCommand { get; set; }
+	}
 }
--- MonikDesktopSolution/Doaking.Core/Oak/Shell.cs
+++ MonikDesktopSolution/Doaking.Core/Oak/Shell.cs
@@ -1,124 +1,121 @@
-﻿using ReactiveUI;
-using ReactiveUI.Fody.Helpers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reactive.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 using Xceed.Wpf.AvalonDock;
 using Xceed.Wpf.AvalonDock.Layout;
 
 namespace MonikDesktop.Oak
 {
-  // http://stackoverflow.com/questions/12493117/wpf-avalondock-v2-0-layoutdocument
-  public class Shell : ReactiveObject
-  {
-    private ReactiveList<IDockingWindow> FWindows;
+	// http://stackoverflow.com/questions/12493117/wpf-avalondock-v2-0-layoutdocument
+	public class Shell : ReactiveObject
+	{
+		private DockingManager FDocker;
+		private LayoutDocumentPane FDocumentPane;
+		private LayoutRoot FLayoutRoot;
+		private LayoutAnchorablePane FLeftPane;
 
-    [Reactive]
-    public IDockingWindow SelectedWindow { get; set; } = null;
+		private readonly Dictionary<Type, Type> FModelViews;
+		private readonly ReactiveList<IDockingWindow> FWindows;
 
-    private DockingManager FDocker = null;
-    private LayoutRoot FLayoutRoot = null;
-    private LayoutDocumentPane FDocumentPane = null;
-    private LayoutAnchorablePane FLeftPane = null;
+		public Shell()
+		{
+			FWindows = new ReactiveList<IDockingWindow>();
+			FModelViews = new Dictionary<Type, Type>();
+		}
 
-    private Dictionary<Type, Type> FModelViews;
+		[Reactive]
+		public IDockingWindow SelectedWindow { get; set; } = null;
 
-    public Shell()
-    {
-      FWindows = new ReactiveList<IDockingWindow>();
-      FModelViews = new Dictionary<Type, Type>();
-    }
+		public void RegisterModelView<MODEL, VIEW>()
+			where MODEL : IDockingWindow
+			where VIEW : UserControl
+		{
+			FModelViews.Add(typeof(MODEL), typeof(VIEW));
+		}
 
-    public void RegisterModelView<MODEL, VIEW>() 
-      where MODEL : IDockingWindow
-      where VIEW : UserControl
-    {
-      FModelViews.Add(typeof(MODEL), typeof(VIEW));
-    }
+		public void AttachDocker(DockingManager aDocker)
+		{
+			FDocker = aDocker;
 
-    public void AttachDocker(DockingManager aDocker)
-    {
-      FDocker = aDocker;
+			FLayoutRoot = new LayoutRoot();
+			FDocker.Layout = FLayoutRoot;
 
-      FLayoutRoot = new LayoutRoot();
-      FDocker.Layout = FLayoutRoot;
+			FDocumentPane = FLayoutRoot.RootPanel.Children[0] as LayoutDocumentPane;
 
-      FDocumentPane = FLayoutRoot.RootPanel.Children[0] as LayoutDocumentPane;
+			FLeftPane = new LayoutAnchorablePane();
+			FLayoutRoot.RootPanel.Children.Insert(0, FLeftPane);
+			FLeftPane.DockWidth = new GridLength(410);
+		}
 
-      FLeftPane = new LayoutAnchorablePane();
-      FLayoutRoot.RootPanel.Children.Insert(0, FLeftPane);
-      FLeftPane.DockWidth = new System.Windows.GridLength(410);
-    }
+		public void ShowTool(IDockingWindow aWindow)
+		{
+			if (FWindows.Contains(aWindow))
+				return;
 
-    public void ShowTool(IDockingWindow aWindow)
-    {
-      if (FWindows.Contains(aWindow))
-        return;
+			var _view = CreateView(aWindow);
+			FWindows.Add(aWindow);
 
-      var _view = CreateView(aWindow);
-      FWindows.Add(aWindow);
+			var _layoutAnchorable = new LayoutAnchorable();
 
-      var _layoutAnchorable = new LayoutAnchorable();
+			aWindow.WhenAnyValue(x => x.Title)
+				.Subscribe(v => _layoutAnchorable.Title = v);
 
-      aWindow.WhenAnyValue(x => x.Title)
-        .Subscribe(v => _layoutAnchorable.Title = v);
+			aWindow.WhenAnyValue(x => x.CanClose)
+				.Subscribe(v =>
+				{
+					_layoutAnchorable.CanHide = v;
+					_layoutAnchorable.CanClose = v;
+				});
 
-      aWindow.WhenAnyValue(x => x.CanClose)
-        .Subscribe(v =>
-        {
-          _layoutAnchorable.CanHide = v;
-          _layoutAnchorable.CanClose = v;
-        });
+			_layoutAnchorable.Content = _view;
 
-      _layoutAnchorable.Content = _view;
+			FLeftPane.Children.Add(_layoutAnchorable);
+		}
 
-      FLeftPane.Children.Add(_layoutAnchorable);
-    }
+		public void ShowDocument(IDockingWindow aWondow)
+		{
+			var _view = CreateView(aWondow);
+			FWindows.Add(aWondow);
 
-    public void ShowDocument(IDockingWindow aWondow)
-    {
-      var _view = CreateView(aWondow);
-      FWindows.Add(aWondow);
+			var _layoutDocument = new LayoutDocument();
 
-      var _layoutDocument = new LayoutDocument();
+			aWondow.WhenAnyValue(x => x.Title)
+				.Subscribe(v => _layoutDocument.Title = v);
 
-      aWondow.WhenAnyValue(x => x.Title)
-        .Subscribe(v => _layoutDocument.Title = v);
+			aWondow.WhenAnyValue(x => x.CanClose)
+				.Subscribe(v => _layoutDocument.CanClose = v);
 
-      aWondow.WhenAnyValue(x => x.CanClose)
-        .Subscribe(v => _layoutDocument.CanClose = v);
+			_layoutDocument.Content = _view;
 
-      _layoutDocument.Content = _view;
+			FDocumentPane.Children.Add(_layoutDocument);
 
-      FDocumentPane.Children.Add(_layoutDocument);
+			_layoutDocument.IsActive = true;
+		}
 
-      _layoutDocument.IsActive = true;
-    }
+		private UserControl CreateView(IDockingWindow aModel)
+		{
+			var _types = aModel.GetType().GetInterfaces();
 
-    private UserControl CreateView(IDockingWindow aModel)
-    {
-      var _types = aModel.GetType().GetInterfaces();
+			Type _type = null;
 
-      Type _type = null;
+			foreach (var it in FModelViews.Keys)
+				if (_types.Contains(it))
+					_type = FModelViews[it];
 
-      foreach (var it in FModelViews.Keys)
-        if (_types.Contains(it))
-          _type = FModelViews[it];
+			if (_type == null)
+				throw new Exception($"Model {aModel} not found");
 
-      if (_type == null)
-        throw new Exception($"Model {aModel} not found");
+			var _view = Activator.CreateInstance(_type) as UserControl;
+			if (_view == null)
+				throw new Exception($"Cant create view for type {_type}");
 
-      var _view = Activator.CreateInstance(_type) as UserControl;
-      if (_view == null)
-        throw new Exception($"Cant create view for type {_type}");
+			_view.DataContext = aModel;
 
-      _view.DataContext = aModel;
-
-      return _view;
-    }
-  }
+			return _view;
+		}
+	}
 }
