0025d4d857481b1e8dbeaaa8f514878f8bd4a148
Totopolis <anikeew@gmail.com>
2017-01-13 17:07:01 +0300

    colors change and dateformat fix

--- MonikDesktopSolution/MonikDesktop/ViewModels/PropertiesViewModel.cs
+++ MonikDesktopSolution/MonikDesktop/ViewModels/PropertiesViewModel.cs
@@ -35,7 +35,7 @@ namespace MonikDesktop.ViewModels
     public IList<TopType> TopTypes { get { return Enum.GetValues(typeof(TopType)).Cast<TopType>().ToList<TopType>(); } }
     public IList<SeverityCutoffType> SeverityCutoffTypes { get { return Enum.GetValues(typeof(SeverityCutoffType)).Cast<SeverityCutoffType>().ToList<SeverityCutoffType>(); } }
     public IList<LevelType> LevelTypes { get { return Enum.GetValues(typeof(LevelType)).Cast<LevelType>().ToList<LevelType>(); } }
-    public IList<string> DatetimeFormats { get { return new string[] { "HH:mm:ss", "dd.MM.YYYY HH:mm:ss", "dd.MM HH:mm:ss" }; } }
+    public IList<string> DatetimeFormats { get { return new string[] { "HH:mm:ss", "dd.MM.yyyy HH:mm:ss", "dd.MM HH:mm:ss" }; } }
 
     [Reactive]
     public string Title { get; set; }
