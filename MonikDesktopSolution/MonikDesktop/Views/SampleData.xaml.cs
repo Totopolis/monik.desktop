8431f80daec211815c3f5a9d3bdefb0dcc91833a
Павел Ожерельев <ozherelev@smartdriving.io>
2018-02-21 15:14:47 +0300

    KeepAlive View

---
+++ MonikDesktopSolution/MonikDesktop/Views/SampleData.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using MonikDesktop.Common.ModelsApp;
+using MonikDesktop.ViewModels.ShowModels;
+
+namespace SampleDatas
+{
+    public class DataGridRows 
+    {
+        public List<KeepALiveItem> KeepALiveList
+        {
+            get
+            {
+                var dateTime = DateTime.Now;
+                return new List<KeepALiveItem>()
+                {
+                    new KeepALiveItem()
+                    {
+                        Created = dateTime,
+                        CreatedStr = dateTime.ToString("hh:mm:ss"),
+                        Instance = new Instance()
+                        {
+                           ID = -1,
+                            Name = "TestInstOk",
+                            Source = new Source()
+                            {
+                                ID = -1,
+                                Name = "TestSourceOk"
+                            },
+                        },
+                        Status = "OK",
+                        StatusIsOk = true
+                    },
+                    new KeepALiveItem()
+                    {
+                        Created = dateTime,
+                        CreatedStr = dateTime.ToString("hh:mm:ss"),
+                        Instance = new Instance()
+                        {
+                           ID = -1,
+                            Name = "TestInstErr",
+                            Source = new Source()
+                            {
+                                ID = -1,
+                                Name = "TestSourceErr"
+                            },
+                        },
+                        Status = "ERROR",
+                        StatusIsOk = false
+                    },
+                };
+            }
+        }
+
+        public KeepAliveModel Model
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
