55a63ec145f85ba86076ccd9bf5344e80e8bc2e7
Михаил Михайлов <m.mikhailov@smartdriving.io>
2020-04-16 12:27:56 +0300

    Fix AvalonDock style

--- MonikDesktopSolution/MonikDesktop/App.xaml.cs
+++ MonikDesktopSolution/MonikDesktop/App.xaml.cs
@@ -1,5 +1,6 @@
-﻿using MonikDesktop.Views;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Controls;
+using MonikDesktop.Views;
 using Ui.Wpf.Common;
 using Ui.Wpf.Common.DockingManagers;
 using Ui.Wpf.Common.ShowOptions;
@@ -12,8 +13,19 @@ namespace MonikDesktop
         {
             base.OnStartup(e);
 
+            var dm = new DefaultDockingManager
+            {
+                DocumentPaneControlStyle = FindResource("AvalonDockThemeCustomDocumentPaneControlStyle") as Style,
+                AnchorablePaneControlStyle = FindResource("AvalonDockThemeCustomAnchorablePaneControlStyle") as Style,
+            };
+            dm.SetResourceReference(Control.BackgroundProperty, "MahApps.Brushes.White");
+
             var shell = UiStarter.Start<IDockWindow>(new Bootstrap(),
-                new UiShowStartWindowOptions {Title = "Monik.Desktop"});
+                new UiShowStartWindowOptions
+                {
+                    Title = "Monik.Desktop",
+                    DockingManager = dm,
+                });
 
             shell.SetContainerWidth(DefaultDockingManager.Tools, new GridLength(350));
             shell.ShowTool<StartupView>(new ViewRequest("startup"));
